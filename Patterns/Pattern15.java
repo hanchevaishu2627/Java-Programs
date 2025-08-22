87f9b5e71b008afb61b2753f7a5c66cfb5d544d7
hanchevaishu2627 <hanchevaishu2627@gmail.com>
2025-08-22 14:20:12 +0530

    Patterns

---
+++ Patterns/Pattern15.java
@@ -0,0 +1,15 @@
+package Patterns;
+
+public class Pattern15 {
+
+	public static void main(String[] args) {
+
+		for(int i=1; i<=5; i++) {
+			for(int j=i; j>=1; j--) {
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
